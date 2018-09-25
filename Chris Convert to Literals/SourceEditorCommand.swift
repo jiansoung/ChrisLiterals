f7958bdf74351c797b90b89a3b0de601c8c1d298
jiansoung <Jian.Soung@outlook.com>
2018-09-26 01:39:55 +0800

    Set code signing & complete perform function

--- Chris Convert to Literals/SourceEditorCommand.swift
+++ Chris Convert to Literals/SourceEditorCommand.swift
@@ -14,6 +14,39 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
         
+        var updatedLineIndexes = [Int]()
+
+        for lineIndex in 0 ..< invocation.buffer.lines.count {
+            let originalLine = invocation.buffer.lines[lineIndex] as! String
+            let literalMapping = [
+                "UIColor(": "#colorLiteral(",
+                "UIImage(named:": "#imageLiteral(resourceName:"
+            ]
+
+            var newLine = originalLine
+            for (old, new) in literalMapping {
+                newLine = newLine.replacingOccurrences(of: old, with: new)
+            }
+
+            if originalLine != newLine {
+                updatedLineIndexes.append(lineIndex)
+                invocation.buffer.lines[lineIndex] = newLine
+            }
+        }
+
+        // Select all lines that received replacements
+
+        let updatedSelections: [XCSourceTextRange] = updatedLineIndexes.map { lineIndex in
+            let lineSelection = XCSourceTextRange()
+            lineSelection.start = XCSourceTextPosition(line: lineIndex, column: 0)
+            lineSelection.end = XCSourceTextPosition(line: lineIndex + 1, column: 0)
+            return lineSelection
+        }
+
+        invocation.buffer.selections.setArray(updatedSelections)
+
+        // Finish the command
+
         completionHandler(nil)
     }
     

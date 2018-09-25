9faa222c1e94e037d76acd26102bd1875afb4b44
jiansoung <Jian.Soung@outlook.com>
2018-09-25 23:32:16 +0800

    Initial Commit

---
+++ ChrisLiterals/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChrisLiterals
+//
+//  Created by songjian on 2018/9/25.
+//  Copyright © 2018 Jian Soung's Studio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
